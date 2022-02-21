1071021cb4d501a76bdc0f7bba8f31eb2bcb30a2
perumalComorins <86822193+perumalComorins@users.noreply.github.com>
2022-02-21 19:19:03 +0530

    General Project setup 21-02-22
    General Project setup 21-02-22

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel='stylesheet' type='text/css' media='screen' href='theme.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/theme.css'>
     <link rel="stylesheet" href="css/responsive.css" />
     <script src='main.js'></script>
 </head>
 <body>
-
+  <h1>Web page</h1>
     
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
